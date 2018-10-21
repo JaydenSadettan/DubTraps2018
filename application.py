736d0cf15e0032cf6801e9ea8cdc465c82334cd1
Jayden Sadettan <JaySadettan@gmail.com>
2018-10-21 06:46:05 -0700

    init

---
+++ application.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, flash
+from werkzeug.utils import secure_filename
+import os
+from ReadImage import ReadImage
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/tmp'
+# app.config['UPLOADED_PHOTOS_DEST'] = 'static/tmp'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['photo']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        return result()
+    return render_template("upload.html")
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    image = []
+    for filename in os.listdir('Static/tmp'):
+        image.append(os.path.join('Static/tmp', filename))
+
+    user_stats = ReadImage(image[0], local=True)
+    os.remove(image[0])
+
+    return str(user_stats.dict)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html,'), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
