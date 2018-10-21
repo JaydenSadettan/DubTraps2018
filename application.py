a9326320e938bc71e53e8d258452a78f50c90556
Jayden <sadettan@cs.washington.edu>
2018-10-21 02:47:31 -0700

    Took out Flask Uploads

--- application.py
+++ application.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template, request, send_from_directory
-# from flaskext.uploads import UploadSet, configure_uploads, IMAGES
+from flask import Flask, render_template, request, flash
+from werkzeug.utils import secure_filename
 import os
-# from ReadImage import ReadImage
+from ReadImage import ReadImage
 
 app = Flask(__name__)
-# photos = UploadSet('photos', IMAGES)
-
+UPLOAD_FOLDER = 'static/tmp'
 # app.config['UPLOADED_PHOTOS_DEST'] = 'static/tmp'
-# configure_uploads(app, photos)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/")
@@ -17,22 +16,23 @@ def home():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    # if request.method == 'POST' and 'photo' in request.files:
-    #     filename = photos.save(request.files['photo'])
-    #     return result()
-    return render_template('upload.html')
+    if request.method == 'POST':
+        f = request.files['photo']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        return result()
+    return render_template("upload.html")
 
 
-# @app.route('/result', methods=['GET', 'POST'])
-# def result():
-#     image = []
-#     for filename in os.listdir('Static/tmp'):
-#         image.append(os.path.join('Static/tmp', filename))
-#
-#     user_stats = ReadImage(image[0], local=True)
-#     os.remove(image[0])
-#
-#     return str(user_stats.dict)
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    image = []
+    for filename in os.listdir('Static/tmp'):
+        image.append(os.path.join('Static/tmp', filename))
+
+    user_stats = ReadImage(image[0], local=True)
+    os.remove(image[0])
+
+    return str(user_stats.dict)
 
 @app.errorhandler(404)
 def page_not_found(e):
